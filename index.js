2a4628d0a0a4c65adc148a7e5d37c680cde4ec27
Aaroneld <merrifield48@gmail.com>
2021-01-24 05:56:19 -0500

    introduced micro-optimizations

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ console.log(canvas.width);
 ctx.fillStyle = "#ffffff";
 ctx.fillRect(0, 0, 100, 300);
 
-function createParticle() {
-    return {
-        x: ~~(Math.random() * canvas.width),
-        y: -~~(Math.random() * canvas.height),
-    };
-}
-
 function returnRandomWindValue() {
     const RAND = Math.random();
 
@@ -32,57 +25,89 @@ function returnRandomWindValue() {
     }
 }
 
+function createParticle() {
+    return {
+        x: ~~(Math.random() * canvas.width),
+        y: -~~(Math.random() * canvas.height),
+        windValue: returnRandomWindValue(),
+    };
+}
+
 let particles = [];
 
 for (i = 0; i < 3000; i++) {
     particles.push(createParticle());
 }
 
-function WindMap(particles) {
-    let windMap = [];
+// function WindMap(particles) {
+//     let windMap = [];
 
-    return function simulateWind(y, i) {
-        if (y < 0) {
-            windMap[i] = returnRandomWindValue();
-        }
+//     return function simulateWind(y, i) {
+//         if (y < 0) {
+//             windMap[i] = returnRandomWindValue();
+//         }
 
-        const RAND = Math.random();
+//         const RAND = Math.random();
 
-        if (RAND > 0.9999) {
-            windMap[i] = returnRandomWindValue();
-        }
+//         if (RAND > 0.9999) {
+//             windMap[i] = returnRandomWindValue();
+//         }
 
-        return windMap[i];
-    };
-}
+//         return windMap[i];
+//     };
+// }
 ctx.lineWidth = 0.2;
 
-const simulateWind = WindMap(particles);
+// const simulateWind = WindMap(particles);
 
 function animation() {
     ctx.clearRect(0, 0, 5000, 5000);
 
     ctx.fillStyle = "white";
 
-    particles = particles.map((particle, index) => {
+    const LENGTH = particles.length;
+
+    for (i = LENGTH - 1; i > -1; i--) {
         ctx.beginPath();
 
         ctx.arc(
-            particle.x,
-            particle.y,
-            index % 2 === 0 ? 1 : 2,
+            particles[i].x,
+            particles[i].y,
+            i % 2 === 0 ? 1 : 2,
             0,
             Math.PI * 2
         );
+
         ctx.fill();
 
-        return particle.y > canvas.height
-            ? createParticle()
-            : {
-                  x: (particle.x += simulateWind(particle.y, index)),
-                  y: particle.y + 0.5,
-              };
-    });
+        particles[i].y > canvas.height
+            ? (particles[i] = createParticle())
+            : (particles[i] = {
+                  x: particles[i].x + particles[i].windValue,
+                  y: particles[i].y + 0.5,
+                  windValue: particles[i].windValue,
+              });
+    }
+
+    // particles = particles.map((particle, index) => {
+    //     ctx.beginPath();
+
+    //     ctx.arc(
+    //         particle.x,
+    //         particle.y,
+    //         index % 2 === 0 ? 1 : 2,
+    //         0,
+    //         Math.PI * 2
+    //     );
+    //     ctx.fill();
+
+    //     return particle.y > canvas.height
+    //         ? createParticle()
+    //         : {
+    //               x: (particle.x += simulateWind(particle.y, index)),
+    //               y: particle.y + 0.5,
+    //           };
+    // });
 
     requestAnimationFrame(animation);
 }
